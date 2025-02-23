d59cce92060d4fd1108eb027681f2db2f42209a7
Naveen <naveen@neuronimbus.com>
2025-02-23 21:07:35 +0530

    Implement pages and navigation with header

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,3 @@
+export default function MealDetails({ params }) {
+  return <h1>Meal Details: {params.slug}</h1>;
+}
---
+++ components/main-header/main-header.js
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import logoImage from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImage} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href={"/meals"}>Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href={"/community"}>Foodie Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
