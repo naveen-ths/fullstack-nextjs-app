806bb03114d55da578b76029aefa8dbccb41cadb
Naveen <naveen@neuronimbus.com>
2025-03-13 22:52:36 +0530

    implement share food and saving it

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -1,7 +1,6 @@
 import MealItem from "./meal-item";
 import classes from "./meals-grid.module.css";
 export default function MealsGrid({ meals }) {
-  console.log(meals)
   return (
     <ul className={classes.meals}>
       {meals.map((meal) => (
